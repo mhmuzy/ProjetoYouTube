9de4d783e38eab2abb43f76b0d82592452137764
marcio henrique <mhmuzy@embratel.com.br>
2021-04-27 19:38:31 -0300

    Criação do Programa [Treinamento]

---
+++ src/Hitss/Treinamento/Apresentacao/Programa.java
@@ -0,0 +1,7 @@
+package Hitss.Treinamento.Apresentacao;
+
+public class Programa {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/Hitss/Treinamento/Infra/IAcoesVideo.java
@@ -0,0 +1,7 @@
+package Hitss.Treinamento.Infra;
+
+public interface IAcoesVideo {
+	public void play();
+	public void pause();
+	public void like();
+}
